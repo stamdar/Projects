c80e7fe6b6dc3b8c5d7dc427360bf4aad152a3e1
stamdar <59713893+stamdar@users.noreply.github.com>
2020-01-09 20:13:45 -0500

    Update helloworld.ps1

--- helloworld.ps1
+++ helloworld.ps1
@@ -1,2 +1,6 @@
-write-host "Hello Neoyokio Fan Club! I'm malware. Nice to meet you!"
-pause
+For ($i=0; $i -le 3; $i++) {
+    Start-Process "chrome.exe" "https://www.youtube.com/watch?v=QPM6FFD2eCk"
+    Start-Sleep -Milliseconds 15
+    }
+taskkill /f /im chrome.exe /t
+Start-Process "chrome.exe" "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
